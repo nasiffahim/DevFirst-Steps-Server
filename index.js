1094fc315b1eedbf9aadc13a5850d1fef750e10c
unknown <nayemk295@gmail.com>
2025-09-17 14:52:36 +0600

    add  all repo get api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;// change   5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { default: axios } = require('axios');
 require('dotenv').config();
 
 //Middlware
@@ -27,10 +28,40 @@ async function run() {
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-
-
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+
+
+
+ 
+ app.get("/all", async (req, res) => {
+  try {
+      //  req query default value
+    const lang = req.query.lang || "javascript";
+    const stars = req.query.stars || 100;  
+    const fork = req.query.fork || 10;    
+ // git api get sending Frontend
+    const response = await axios(
+      `https://api.github.com/search/repositories?q=language:${lang}+stars:>${stars}+forks:>${fork}&sort=stars&order=desc`,
+       {
+        headers: {
+          "User-Agent": "my-app"
+        }
+      }
+    );
+    if (!response.ok) {
+      return res.status(response.status).json({ message: "GitHub API error" });
+    }
+     const data = response.data;
+     res.json(data.items);
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({
+      message: "Failed to fetch repositories",
+      error: error.message,
+    });
+  }
+});
+     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -38,8 +69,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
   res.send('Open Source Server Running!!!');
